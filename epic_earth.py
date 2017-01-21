b3707ffdd69828edf0664e17caa5b0bbc059775c
Tian Shixiong <tiansworld@fedoraproject.org>
2017-01-21 22:41:49 +0800

    Moved the comment before else statement to the top of if statement, so it won't cause invalid syntax error

--- epic_earth.py
+++ epic_earth.py
@@ -5,42 +5,42 @@ import urllib.parse
 import os
 
 '''Use urllib to retrieve available dates page'''
-'''Available dates page is read and loaded by json.loads to variable date.'''
-tempdate, headers = urllib.request.urlretrieve\
-       ('http://epic.gsfc.nasa.gov/api/images.php?available_dates')
-html = open(tempdate,encoding="utf-8")
+'''Available dates page is read and loaded by json.loads to variable
+date.'''
+tempdate, headers = urllib.request.urlretrieve(
+        'http://epic.gsfc.nasa.gov/api/images.php?available_dates')
+html = open(tempdate, encoding="utf-8")
 date = json.loads(html.read())
 html.close()
 
-'''Variable already is a file list of current directory. It is used to check
-whether a image has been downloaded. Default place is the directory at which
-this script locates.'''
+'''Variable already is a file list of current directory. It is used to
+check whether a image has been downloaded. Default place is the directory
+at which this script locates.'''
 already = os.listdir("./")
 
-'''To save running time of the script, first to compare the available dates'''
-'''with the local date file.'''
+'''To save running time of the script, first to compare the available
+dates with the local date file.'''
+'''If the 'datefile' doesn't exist, then it means you haven't download
+any images, it will begin to download all the images. So don't delete
+datefile, otherwise, the script will try to check every dates available
+on the nasa server, and compare the image names within each day with your
+local image names, this will spend a lot time even no image is need to
+download.'''
 if 'datefile' in already:
-    date_old = open("./datefile",mode='r',encoding='utf-8')
+    date_old = open("./datefile", mode='r', encoding='utf-8')
     datefile = date_old.read()
     date_old.close()
     if len(datefile) < len(date):
         date = date[len(datefile):]
-        date_old = open("./datefile",mode="a",encoding='utf-8')
+        date_old = open("./datefile", mode="a", encoding='utf-8')
         date_old.write(str(date))
         date_old.close()
-        
-'''If the 'datefile' doesn't exist, then it means you haven't download any'''
-'''images, it will begin to download all the images. So don't delete '''
-'''datefile, otherwise, the script will try to check every dates available on'''
-'''the nasa server, and compare the image names within each day with your local'''
-'''image names, this will spend a lot time even no image is need to download.'''
 else:
-    datefile = open("./datefile",mode="w",encoding='utf-8')
+    datefile = open("./datefile", mode="w", encoding='utf-8')
     datefile.write(str(date))
     datefile.close()
-    
-'''From here, the script will try to download the images that taken by each
-dates'''    
+'''From here, the script will try to download the images that taken by
+each dates'''
 for i in date:
     url = 'http://epic.gsfc.nasa.gov/api/images.php?date='+i
     temp_data, headers = urllib.request.urlretrieve(url)
@@ -48,8 +48,7 @@ for i in date:
     data = json.loads(data.read())
     for f in data:
         if str(f['image']+'.jpg') not in already:
-            urllib.request.urlretrieve(\
-                "http://epic.gsfc.nasa.gov/epic-archive/jpg/"\
-                    +f['image']+'.jpg',str(f['image']+'.jpg'))
-            
+            urllib.request.urlretrieve(
+                "http://epic.gsfc.nasa.gov/epic-archive/jpg/" +
+                f['image'] + '.jpg', str(f['image']+'.jpg'))
 print("All downloaded. Enjoy!")
